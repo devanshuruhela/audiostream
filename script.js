3f2ed844f8a07968f4ecf6c462031037b4d652ea
Devanshu Ruhela <devanshuruhela2.0@gmail.com>
2021-08-26 01:22:29 +0530

    design done

---
+++ script.js
@@ -0,0 +1,5 @@
+const audioElement = document.querySelector('audio');
+
+const play = document.getElementById('play');
+const prev = document.getElementById('prev');
+const next = document.getElementById('next')
