3946aa51e0ab821b884cac833a6f2f74bf33df94
Devanshu Ruhela <devanshuruhela2.0@gmail.com>
2021-08-26 01:42:14 +0530

    play pause set

--- script.js
+++ script.js
@@ -3,3 +3,23 @@ const audioElement = document.querySelector('audio');
 const play = document.getElementById('play');
 const prev = document.getElementById('prev');
 const next = document.getElementById('next')
+
+let isplaying = false;
+
+function playmusic()
+{
+  isplaying = true;
+  audioElement.play();
+  play.classList.replace('fa-play' , 'fa-pause')
+  play.setAttribute('title' , 'Pause')
+}
+
+function pausemusic()
+{
+  isplaying = false;
+  audioElement.pause();
+  play.classList.replace("fa-pause", "fa-play");
+  play.setAttribute("title", "Play");
+}
+
+play.addEventListener('click' ,()=> (isplaying ? pausemusic() : playmusic()));
